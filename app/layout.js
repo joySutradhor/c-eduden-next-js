ca2cfe00d620d85513f2845910fce9d9be4ddaed
joySutradhor <joysutradhorcmt@gmail.com>
2025-09-25 19:12:35 +0600

    right click off

--- app/layout.js
+++ app/layout.js
@@ -1,36 +1,39 @@
-
+// app/layout.js
 import './globals.css'
 import Footer from '@/components/shared/Footer/Footer'
 import Navbar from '@/components/navbar/Navbar'
 
 import { Lexend } from 'next/font/google'
 import localFont from 'next/font/local'
+import ClientWrapper from './ClientWrapper'
+
 
 const lexend = Lexend({
   subsets: ['latin'],
-  variable: '--font-lexend', 
+  variable: '--font-lexend'
 })
 
 const gucina = localFont({
-  src: './font/gucina.bold.otf', 
-  variable: '--font-gucina', 
+  src: './font/gucina.bold.otf',
+  variable: '--font-gucina'
 })
 
 export const metadata = {
   title: 'eduden | Home',
-  description: 'Best course platform'
+  description: 'Best course platform in kolkata'
 }
 
-export default function RootLayout ({ children }) {
+export default function RootLayout({ children }) {
   return (
-    <html lang='en' data-arp=''>
+    <html lang="en">
       <body
-        cz-shortcut-listen='true'
-        className={`${lexend.variable} ${gucina.variable} antialiased  bg-[#010101] text-white`}
+        className={`${lexend.variable} ${gucina.variable} antialiased bg-[#010101] text-white`}
       >
-        <Navbar/>
-        {children}
-        <Footer/>
+        <ClientWrapper>
+          <Navbar />
+          {children}
+          <Footer />
+        </ClientWrapper>
       </body>
     </html>
   )
